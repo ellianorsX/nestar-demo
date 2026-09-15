3b32f789742751b8cf48c37919520ec3b501ab5e
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-09-15 12:39:47 +0900

    feat: publish homepage screen components

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -4,6 +4,9 @@ import Top from "../Top";
 import Footer from "../Footer";
 import useDeviceDetect from "../../hooks/useDeviceDetect";
 import HeaderFilter from "../homepage/HeaderFilter";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
